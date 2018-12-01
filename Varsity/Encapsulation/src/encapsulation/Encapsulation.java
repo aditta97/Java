6ddc3ceeb7de13d818255580c2e06494045f89a7
Aditta Chakraborty <adittasarma@gmail.com>
2018-12-01 16:00:05 +0600

    Update Encapsulation.java

--- Varsity/Encapsulation/src/encapsulation/Encapsulation.java
+++ Varsity/Encapsulation/src/encapsulation/Encapsulation.java
@@ -18,8 +18,7 @@ public class Encapsulation {
     public static void main(String[] args) {
        ABC A = new ABC();
        A.Set_X(10);
-        System.out.println(A.Get_X());
-       //x = 10;
+       System.out.println(A.Get_X());
     }
     
 }
