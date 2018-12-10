658763a323f7db9ece6c8586b872b49e8f80e1e8
Aditta Chakraborty <adittasarma@gmail.com>
2018-12-10 12:45:23 +0600

    Class 4

---
+++ USSL/Class 4/Review/src/review/Review.java
@@ -0,0 +1,21 @@
+
+package review;
+
+public class Review {
+    static void add(){
+    
+    }
+
+    public static void main(String[] args) {
+        String a="hello";
+
+        StringBuffer ob=new StringBuffer("hello");
+        //println();
+        System.out.println(a);
+        
+        ob.reverse();
+        System.out.println(ob);
+
+    }
+    
+}
