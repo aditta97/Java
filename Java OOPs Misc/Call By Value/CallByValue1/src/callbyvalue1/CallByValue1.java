05effe589de70425b96c268ccd58cfe47d6eab58
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-15 03:38:45 +0600

    Call By Value1

--- Java OOPs Misc/Call By Value/CallByValue1/src/callbyvalue1/CallByValue1.java
+++ Java OOPs Misc/Call By Value/CallByValue1/src/callbyvalue1/CallByValue1.java
@@ -10,12 +10,23 @@ package callbyvalue1;
  * @author adittachakraborty
  */
 public class CallByValue1 {
+    
+    int data = 50;
+    
+    void change (int data) {
+        data = data + 100;  //changes will be in the local variable only
+        System.out.println("In change method: " + data);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CallByValue1 cbv = new CallByValue1();
+        
+        System.out.println("befor change: " + cbv.data);
+        cbv.change(500);
+        System.out.println("after change: " + cbv.data);
     }
     
 }
