340a8e9f2574d89e0fc072dea38192fbb00c1d9d
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-16 04:11:00 +0600

    Java String

---
+++ Java String/Substring/src/substring/Substring.java
@@ -0,0 +1,19 @@
+
+package substring;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class Substring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s = "Aditta Chakraborty";
+        System.out.println(s.substring(4));
+        System.out.println(s.substring(0, 12));
+    }
+    
+}
