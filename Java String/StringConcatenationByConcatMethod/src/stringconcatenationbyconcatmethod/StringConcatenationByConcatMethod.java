a5b3a8fe5daf6cdaf012f8dc6f96a25835b61d63
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-16 03:35:33 +0600

    Java String

---
+++ Java String/StringConcatenationByConcatMethod/src/stringconcatenationbyconcatmethod/StringConcatenationByConcatMethod.java
@@ -0,0 +1,21 @@
+
+package stringconcatenationbyconcatmethod;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class StringConcatenationByConcatMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s1 = "Aditta";
+        String s2 = " Chakraborty";
+        String s3 = s1.concat(s2);
+        
+        System.out.println(s3);
+    }
+    
+}
---
+++ Java String/StringConcatenationByPlusOperator/src/stringconcatenationbyplusoperator/StringConcatenationByPlusOperator.java
@@ -0,0 +1,23 @@
+
+package stringconcatenationbyplusoperator;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class StringConcatenationByPlusOperator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s = "Aditta" + " Chakraborty";
+        
+        System.out.println(s);
+        
+        String text = 50 + 30 + " Aditta " + 40 + 40;
+        
+        System.out.println(text);
+    }
+    
+}
