90873b2a1f4eb60448cc7197481155034b8b5b57
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-17 00:38:26 +0600

    Java String

---
+++ Java String/StringBuilder Class/ReverseMethod/src/reversemethod/ReverseMethod.java
@@ -0,0 +1,20 @@
+
+package reversemethod;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class ReverseMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.reverse();
+        
+        System.out.println(sb);
+    }
+    
+}
