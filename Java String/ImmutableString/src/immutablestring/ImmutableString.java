5dbac198dc0b3fd34d86415910bb4775e6f0c303
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-16 16:12:00 +0600

    Java String

--- Java String/ImmutableString/src/immutablestring/ImmutableString.java
+++ Java String/ImmutableString/src/immutablestring/ImmutableString.java
@@ -1,11 +1,5 @@
 package immutablestring;
 
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /**
  *
  * @author adittachakraborty
@@ -16,25 +10,14 @@ public class ImmutableString {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Path path = Paths.get("data");
-
-        try {
-            Path newDir = Files.createDirectory(path);
-        } catch (FileAlreadyExistsException e) {
-            // the directory already exists.
-        } catch (IOException e) {
-            //something else went wrong
-            e.printStackTrace();
-        }
-
-//        String s = "Sachin";
-//        s.concat(" Tendulkar"); //concat() method appends the string at the end
-//        System.out.println(s);
-//        
-//        System.out.println();
-//        
-//        s = s.concat(" Tendulkar"); //still sachin object is not modified.
-//        System.out.println(s);
+        String s = "Sachin";
+        s.concat(" Tendulkar"); //concat() method appends the string at the end
+        System.out.println(s);
+        
+        System.out.println();
+        
+        s = s.concat(" Tendulkar"); //still sachin object is not modified.
+        System.out.println(s);
     }
 
 }
