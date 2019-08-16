2eb80de2de866016368d5cf4c8706037664a2451
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-16 16:29:09 +0600

    Java String

---
+++ Java String/StringBuffer Class/ReverseMethod/src/reversemethod/ReverseMethod.java
@@ -0,0 +1,20 @@
+
+package reversemethod;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class ReverseMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.reverse();   // It reverse the String
+        
+        System.out.println(sb); // Prints olleH
+    }
+    
+}
