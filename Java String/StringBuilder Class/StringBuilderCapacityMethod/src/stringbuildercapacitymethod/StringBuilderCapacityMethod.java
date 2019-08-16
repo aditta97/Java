7a1b4afcfe9752cbb25001be89f2524ce5856aca
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-17 01:46:25 +0600

    Java String

---
+++ Java String/StringBuilder Class/StringBuilderCapacityMethod/src/stringbuildercapacitymethod/StringBuilderCapacityMethod.java
@@ -0,0 +1,24 @@
+
+package stringbuildercapacitymethod;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class StringBuilderCapacityMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.capacity()); //default 16
+        
+        sb.append("Hello");
+        System.out.println(sb.capacity()); //now 16
+        
+        sb.append("Java is my favourite language");
+        System.out.println(sb.capacity());  //now (16*2)+2=34 i.e (oldcapacity*2)+2
+    }
+    
+}
