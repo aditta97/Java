b978f94248a5107390d3e5b7f191a9379653b796
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-17 00:27:23 +0600

    Java String

---
+++ Java String/StringBuilder Class/AppendMethod/src/appendmethod/AppendMethod.java
@@ -0,0 +1,20 @@
+
+package appendmethod;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class AppendMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" Java");
+        
+        System.out.println(sb);
+    }
+    
+}
