6a4638e5337fd96d6e126c159c7ba9ca727848dc
Aditta Chakraborty <adittasarma@gmail.com>
2019-08-27 04:43:13 +0600

    Java Date

---
+++ Java Date/Duration/GetMethod/src/getmethod/GetMethod.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getmethod;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+/**
+ *
+ * @author adittachakraborty
+ */
+public class GetMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Duration d = Duration.between(LocalTime.NOON, LocalTime.MAX);
+        System.out.println(d.get(ChronoUnit.SECONDS));
+    }
+    
+}
